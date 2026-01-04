e644cbac78e0933140b1ecb2a6b0e8cff484080f
truongdubaix <truongcss1220@gmail.com>
2026-01-04 16:59:34 +0700

    fix FE

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import MainLayout from "../layout/MainLayout.jsx";
+// import MainLayout from "../layout/MainLayout";
 import Home from "../pages/Home.jsx";
 import AtHome from "../pages/AtHome.jsx";
+import MainLayout from "../components/layout/MainLayout.jsx";
 
 export default function AppRoutes() {
   return (
